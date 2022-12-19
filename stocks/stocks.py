8005634eaa4bd5f40cb0865dc45616aded8cbe74
Paul Bosker <pbosker@hotmail.com>
2022-12-19 08:51:52 +0000

    updated weather_scrape.csv on 12192022

--- stocks/stocks.py
+++ stocks/stocks.py
@@ -1,22 +1,15 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 import datetime
 
+with open("/home/paul/python/stocks/stock_prices.csv", newline='') as f:
+  reader = csv.reader(f)
+  for row in reader:
+#    print(str(row))
+    break
+
 url = "https://www.marketwatch.com/investing/stock/"
-codes=["SCS", "HOG", "AAPL", "MSFT", "GOOG", "AMZN", "WWW", "SPTN", "TSLA", "PLUG", "ARRY", "COMP", "AMGN", "IBM", "CAT", "MMM", "XOM", "ABBV"]
-#listx = ","
-
-def find_num_of_symbols():
-    number_of_symbols = len(codes)
-    return number_of_symbols
-
-def get_stock_name(c):
-    result = requests.get(url + c)
-    soup = BeautifulSoup(result.text, "html.parser")
-    lists = soup.find_all('div',class_="element element--company")
-    for list in lists:
-        long_name = list.find('h1', class_="company__name").text
-        return long_name
 
 def get_stock_price(c):
     result = requests.get(url + c)
@@ -28,21 +21,17 @@ def get_stock_price(c):
 
 dn = datetime.datetime.now()
 outfl = str(dn) + ", "
-number_of_symbols = find_num_of_symbols()
 
-#for x in range(number_of_symbols):
-#    listx = listx + codes[x] + ", "
-
-for name in codes:
-#    long_name = get_stock_name(name)
-    price = get_stock_price(name)
-    outfl = outfl + str(price) + ", "
+for name in row:
+    if (str(name) == '') or (str(name) == ' '):
+        continue
+    else:
+        price = get_stock_price(name)
+        outfl = outfl + str(price) + ", "
 
 f = open("/home/paul/python/stocks/stock_prices.csv", "a+")
 f.seek(0)
 data = f.read(40)
-#f.write(str(listx))
-#f.write("\n")
 f.write(str(outfl))
 f.write("\n")
 f.close()
