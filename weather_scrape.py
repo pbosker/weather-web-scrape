2ffc2c71b3a139aad25b58dcfe01abbeb5f89a2f
Paul Bosker <pbosker@hotmail.com>
2022-12-24 00:45:59 +0000

    updated weather_scrape.csv on 12242022

--- weather_scrape.py
+++ weather_scrape.py
@@ -1,24 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
+# this program is designed to extrat weather information from a website.    The website is specified below
+
 url = "https://paulbosker.com/2022/08/21/weather/"
+
+#initialize variables. 
 outfl = ""
 have_t = "N"
 have_h = "N"
-have_p = "N"
+
+# The below variables must be initialized here because if the value cannot be determined from the website, the program terminates when the variable is attempted to be printed.    
+# By declaring the variables here, this problem is eliminated
 t = 0.00
 w = 0.00
 h = 0.00
-p = 0.00
-r = 0.00
 
 result = requests.get(url)
 soup = BeautifulSoup(result.text, "html.parser")
 lists = soup.find_all('div',class_="lws-widget-row")
+
+# Get current date/time from website (d & dt)
 dt = soup.find_all(class_="elementor-shortcode")[0]
 for d in dt:
     pass
 
+# Get temperature (t) and wind (w)
 for list1 in lists:
     node = list1.find('div', class_="lws-widget-big-value")
     if node is not None:
@@ -29,9 +36,7 @@ for list1 in lists:
                 have_t = "Y"
         elif list1.find('div', class_="lws-widget-big-unit").text == "mph":
             w = num
-        elif list1.find('div', class_="lws-widget-big-unit").text == "in/h":
-            r = num
-
+# Get humidity (h)
 for list2 in lists:
     node = list2.find('div', class_="lws-widget-med-value")
     if node is not None:
@@ -40,12 +45,11 @@ for list2 in lists:
             if have_h == "N":
                 h = num
                 have_h = "Y"
-        elif list2.find('div', class_="lws-widget-med-unit").text == "inHg":
-            if have_p == "N":
-                p = num
-                have_p = "Y"
+
+# build output format
 outfl = str(d) + "," + str(t) + "," + str(w) + "," + str(h)
 
+# write to file
 f = open("/home/paul/python/weather_scrape.csv", "a+")
 f.seek(0)
 data = f.read(40)
